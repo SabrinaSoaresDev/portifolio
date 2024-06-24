4f48740e74ab78da45ef97644bac718769843043
SabrinaSoaresDev <sabrinams9.ss@gmail.com>
2024-06-24 10:32:42 -0300

    atualizando projetos

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,7 +5,15 @@ import { IoIosRocket } from "react-icons/io";
 import { IoLogoJavascript } from "react-icons/io5";
 import perfil from '../../assets/perfil2.jpeg';
 import foto from '../../assets/foto.jpg';
+import chamados from '../../assets/chamado .png';
+import filmes from '../../assets/filme.png';
+import landing from '../../assets/landigin.png';
+import barbearia from '../../assets/barbearia.png';
+import calculadora from '../../assets/calculadoraIMC.png';
+import cardapio from '../../assets/cardapio.png';
+import link from '../../assets/link.png';
 import './home.css';
+import { Link } from "react-router-dom";
 
 export default function Home(){
     return(
@@ -22,9 +30,9 @@ export default function Home(){
                     <button >Download Curriculo</button>
                 </div>
                 <div className="social">
-                <FaInstagram size={25}  />
-                <FaLinkedin size={25} />
-                <FaGithub size={25} />
+                <Link to="https://www.instagram.com/sabrinams.9/" target="_blank"><FaInstagram size={25}  /></Link>
+                <Link to="https://www.linkedin.com/in/sabrina-moreira-soares-caires/" target="_blank"><FaLinkedin size={25} /></Link>
+                <Link to="https://github.com/SabrinaSoaresDev" target="_blank"><FaGithub size={25} /></Link>
                 </div>
             </section>
             <section>
@@ -73,10 +81,71 @@ export default function Home(){
                 </li>
             </ul>
             </section>
-            <section>
-                <h1>Projetos</h1>
-                <h4>Gestão de Chamados </h4>
-                <p></p>
+            <section className="projetos">
+            <h1 className="titulo">Projetos</h1>
+            <div className="projetos-container">
+              <img src={chamados} alt="sistema de chamado" className="projetos-img"  />
+                <div className="projetos-content">
+                    <h4>Gestão de Chamados</h4>
+                    <p>falando sobre o projeto</p>
+                    <Link to="https://chamado-cofarminas-pjzq5nyci-sabrinasoaresdev.vercel.app/" target="_blank">Visualizar</Link>
+                    <Link to="https://github.com/SabrinaSoaresDev/chamado-cofarminas.git" target="_blank">Github</Link>
+                </div>
+              </div>
+              <div className="projetos-container">
+              <img src={cardapio} alt="sistema de chamado" className="projetos-img"  />
+                <div className="projetos-content">
+                    <h4>Cardapio Online</h4>
+                    <p>falando sobre o projeto</p>
+                    <Link to="https://cardapio-beta-ten.vercel.app/" target="_blank">Visualizar</Link>
+                    <Link to="https://github.com/SabrinaSoaresDev/Cardapio.git" target="_blank">Github</Link>
+                </div>
+              </div>
+              <div className="projetos-container">
+              <img src={link} alt="sistema de chamado" className="projetos-img"  />
+                <div className="projetos-content">
+                    <h4>Encurtamento de Link</h4>
+                    <p>falando sobre o projeto</p>
+                    <Link to="https://sujeito-link-sabrinasoaresdev.vercel.app/" target="_blank">Visualizar</Link>
+                    <Link to="https://github.com/SabrinaSoaresDev/SujeitoLink.git" target="_blank">Github</Link>
+                </div>
+              </div>
+              <div className="projetos-container">
+              <img src={filmes} alt="sistema de chamado" className="projetos-img"  />
+                <div className="projetos-content">
+                    <h4>Filmes em Cartazes</h4>
+                    <p>falando sobre o projeto</p>
+                    <Link to="https://filme-poster.netlify.app/" target="_blank">Visualizar</Link>
+                    <Link to="https://github.com/SabrinaSoaresDev/filme_poster.git" target="_blank">Github</Link>
+                </div>
+              </div>
+              <div className="projetos-container">
+              <img src={landing} alt="sistema de chamado" className="projetos-img"  />
+                <div className="projetos-content">
+                    <h4>Landing-Page</h4>
+                    <p>falando sobre o projeto</p>
+                    <Link to="https://pagina-inicial.vercel.app/" target="_blank">Visualizar</Link>
+                    <Link to="https://github.com/SabrinaSoaresDev/Landing-Page.git" target="_blank">Github</Link>
+                </div>
+              </div>
+              <div className="projetos-container">
+              <img src={barbearia} alt="sistema de chamado" className="projetos-img"  />
+                <div className="projetos-content">
+                    <h4>Landing-Barbearia</h4>
+                    <p>falando sobre o projeto</p>
+                    <Link to="https://landpage-barbearia-git-main-sabrinasoaresdev.vercel.app/" target="_blank">Visualizar</Link>
+                    <Link to="https://github.com/SabrinaSoaresDev/Landpage_barbearia.git" target="_blank">Github</Link>
+                </div>
+              </div>
+              <div className="projetos-container">
+              <img src={calculadora} alt="sistema de chamado" className="projetos-img"  />
+                <div className="projetos-content">
+                    <h4>Calculadora IMC</h4>
+                    <p>falando sobre o projeto</p>
+                    <Link to="https://calculadora-olv6cu2mi-sabrinasoaresdev.vercel.app/" target="_blank">Visualizar</Link>
+                    <Link to="https://github.com/SabrinaSoaresDev/CalculadoraIMC.git" target="_blank">Github</Link>
+                </div>
+              </div>
             </section>
         </>
     )
